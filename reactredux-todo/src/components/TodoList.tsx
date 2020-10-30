971fa2172616d2412c39a36b84e01da9b36af5a2
keithh <ckeithkh@gmail.com>
2020-10-29 17:05:07 -0700

    creating containers for sfc's

--- reactredux-todo/src/components/TodoList.tsx
+++ reactredux-todo/src/components/TodoList.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Todo, AppState } from 'types';
 
-// typing connect requires a state, dispatch, and own props
+// typing connect requires a state, dispatch, and own props. Connected
 
-interface Props {
+interface StateProps {
   todoList: Array<Todo>;
   // deleteTodo: (id: number) => void;
 }
 
-const TodoList: React.FC<Props> = ({ todoList }) => {
-  console.log(todoList);
+// type AllProps = StateProps | DispatchProps | OwnProps;
+
+const TodoList = (props: StateProps) => {
+  console.log(props.todoList);
   return (
     <div>
-      {todoList && todoList.length ? (
-        todoList.map((todo: Todo, index: number) => {
-          return <p key={index}>{todo}</p>;
-        })
+      {props.todoList && props.todoList.length ? (
+        props.todoList.map((todo: Todo, index: number) => <p key={index}>{todo.text}</p>)
       ) : (
         <p>Your Todolist is empty!</p>
       )}
@@ -26,9 +26,9 @@ const TodoList: React.FC<Props> = ({ todoList }) => {
 
 //selects which part of redux state you want, and returns that part, called everytime store changes,
 //like useSelector. first argument is redux state, second is ownProps
-const mapStateToProps = (state: AppState): Props => {
+const mapStateToProps = (state: AppState): StateProps => {
   return { todoList: state.todoList };
 };
 
-export default connect<Props, null, {}, AppState>(mapStateToProps)(TodoList);
+export default connect<StateProps, null, {}, AppState>(mapStateToProps)(TodoList);
 // export default connect<StateProps, {}, OwnProps>(mapStateToProps)(TodoList);
